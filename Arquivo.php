ab27bdf20374fce0d74cc01621b5cb0960acf645
@Fagnerdin <fag.dasilva@gmail.com>
2017-09-05 10:23:26 -0300

    Salva relatorio
    ...que gerou um resultado em JSON

--- Arquivo.php
+++ Arquivo.php
@@ -4,11 +4,14 @@ class Arquivo extends CI_Controller {
     
     /** SALVA ARQUIVO DE DADOS */
     public function index() {
+        // recebe os dados do javascript
         $resultado = $this->input->post('Dados');
         if(!empty($resultado)){
             $data = $resultado;
+            // Cria um nome ficticio
             $fname = mktime() . ".txt";
 
+            // Arquivo, escreve, fecha
             $file = fopen("includes/" .$fname, 'w');
             fwrite($file, $data);
             fclose($file);
